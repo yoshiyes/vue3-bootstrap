822a93e4fea739efe0b021bb2dcd32562db77515
Karim <20050570+yoshiyes@users.noreply.github.com>
2021-05-30 14:20:50 +0200

    Continue

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,16 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  <Button>ffffffffffffffffffff</Button>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
-import HelloWorld from './components/HelloWorld.vue'
+import { defineComponent } from "vue";
+import Button from "./components/Button/Button.vue";
 
 export default defineComponent({
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-})
+  name: "App",
+  components: { Button },
+});
 </script>
 
 <style>
---
+++ src/components/Button/Button.vue
@@ -0,0 +1,11 @@
+<template>
+  <button>fff</button>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "b-button",
+});
+</script>
